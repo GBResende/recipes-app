53ed3594e9e0768f8c22e1f637b070630ced5b29
Leticia Andrade <84607458+letsandrade@users.noreply.github.com>
2022-04-05 12:01:41 -0300

    [let | grupo 5]profile page finalizada e favorites page iniciada (#506)
    * profile page finalizada e favorites page iniciada
    
    * changes made before pull

--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+// import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import ProductHeader from '../components/ProductHeader';
 
-const FavoritesRecipes = () => (
-  <div><Header title="Favorite Recipes" /></div>
-);
+function FavoritesRecipes() {
+  // const history = useHistory();
+  const storedFavs = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  console.log('storedFavs', storedFavs);
+
+  return (
+    <div>
+      <Header
+        title="Favorite Recipes"
+        showSearchButton={ false }
+        setIsSearchBarVisible={ () => false }
+        isSearchBarVisible={ false }
+      />
+      <p>FavoritesRecipes</p>
+      <div>
+        {storedFavs && storedFavs.map((storedFav) => (
+          <div
+            key={ storedFav.name }
+          >
+            <ProductHeader
+              productID={ storedFav.id }
+              image={ storedFav.image }
+              name={ storedFav.name }
+              category={ storedFav.category }
+              linkRecipe={ `/${storedFav.type}s/${storedFav.id}` }
+              nationality={ storedFav.nationality }
+              alcoholic={ storedFav.alcoholicOrNot }
+              type={ storedFav.type }
+
+            />
+          </div>
+        ))}
+
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 export default FavoritesRecipes;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Profile = () => (
-  <>
-    <Header title="Profile" />
-    <div>Profile</div>
-    <p>exibir header e footer </p>
-    <p>email do usuario-pegar no local storage </p>
-    <button type="button">
-      Done Recipes
-    </button>
-    <button type="button">
-      Favorite Recipes
-    </button>
-    <button type="button">
-      Logout
-    </button>
-    <Footer />
-  </>
-);
+function Profile() {
+  const storedLocalEmail = JSON.parse(localStorage.getItem('user')).email;
+  const history = useHistory();
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+  const handleBtnDone = () => {
+    history.push('/done-recipes');
+  };
+  const handleBtnFav = () => {
+    history.push('/favorite-recipes');
+  };
+
+  return (
+    <div>
+      <Header
+        title="Profile"
+        showSearchButton={ false }
+        setIsSearchBarVisible={ () => false }
+        isSearchBarVisible={ false }
+      />
+      <p>exibir header e footer </p>
+      <h3 data-testid="profile-email">{ storedLocalEmail }</h3>
+      <button
+        type="button"
+        onClick={ handleBtnDone }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ handleBtnFav }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ handleLogout }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
+      <Footer />
+    </div>
+  );
+}
 
 export default Profile;
